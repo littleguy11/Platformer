07ef9d5b8f8a699c2560f394a19925b7d7cb0211
LittleGuy11 <computerpro108@gmail.com>
2023-04-26 17:44:51 +0200

    Made Charicter knite

--- Simple_Platformer/Assets/Scripts/PlayerMovement.cs
+++ Simple_Platformer/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private CapsuleCollider2D coll;
+    private SpriteRenderer Sprite;
+
+    private float dirX = 0f;
 
     [SerializeField] private LayerMask jumpableGround;
 
@@ -14,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<CapsuleCollider2D>();
+        Sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,15 +26,24 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dirX * 7, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, 14f);
+
+        }
+
+        if (dirX > 0f)
+        {
+            Sprite.flipX = true;
+        }
+        if (dirX < 0f)
+        {
+            Sprite.flipX = false;
         }
     }
 
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .5f, jumpableGround);
-    }
+
+
+    private bool IsGrounded => Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .5f, jumpableGround);
 }
     
