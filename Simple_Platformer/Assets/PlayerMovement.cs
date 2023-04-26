672cef89a788ef7e26ecce3024d1fa751db98334
LittleGuy11 <computerpro108@gmail.com>
2023-04-26 13:56:57 +0200

    simple setup commit

---
+++ Simple_Platformer/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private CapsuleCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 7, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .5f, jumpableGround);
+    }
+}
+    
